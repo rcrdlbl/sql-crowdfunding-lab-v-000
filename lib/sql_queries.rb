72f3a086f5f6de027f2e3bdd42a0b90648437359
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-03 23:33:34 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,11 @@ GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount
+FROM pledges
+INNER JOIN users ON pledges.user_id = users.id
+GROUP BY users.name
+ORDER BY pledges.amount, users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
