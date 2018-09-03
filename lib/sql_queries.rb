a38b0dda05a2726a5cdcf9be06804a0ffa22b997
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-03 23:55:29 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,13 +28,23 @@ GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount)
+FROM pledges
+INNER JOIN users ON pledges.user_id = users.id
+GROUP BY users.name
+ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount
+FROM pledges
+LEFT JOIN projects ON pledges.project_id = projects.id
+WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount)
+FROM pledges
+INNER JOIN projects ON pledges.project_id = projects.id
+GROUP BY projects.category HAVING projects.category = 'books'"
 end
