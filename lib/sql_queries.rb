7f1b858c747fdbc955d6f95d905467d81d2e0440
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-03 23:50:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,8 +37,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount
-FROM pledges
-INNER JOIN projects ON pledges.project_id = projects.id
+FROM projects
+INNER JOIN pledges ON pledges.project_id = projects.id
 WHERE projects.category = 'music'
 GROUP BY projects.category"
 end
